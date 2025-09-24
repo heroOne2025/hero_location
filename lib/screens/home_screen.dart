6abfa0fa55f7dd4b2a10fa567e016ccbbde4e783
abdelrahman <hone83344@gmail.com>
2025-09-24 13:55:46 +0300

    Implement bottom navigation and refactor HomeScreen to use tabs for navigation between Home and Profile

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,20 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:hero_location/core/utils/app_colors.dart';
-import 'package:hero_location/widgets/empty_home_screen.dart';
+import 'package:hero_location/screens/tabs/home_tab.dart';
+import 'package:hero_location/screens/tabs/profile_tab.dart';
+import 'package:hero_location/widgets/bottom_nav.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   static String routeName = '/home';
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int selectedIndex = 0;
+  List<Widget> tabs = [const HomeTab(), const ProfileTab()];
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('My clints'),
+        title: Text(selectedIndex == 0 ? 'Home' : 'Profile'),
         backgroundColor: AppColors.primryColor,
         centerTitle: true,
       ),
-      body: EmptyHomeScreen(),
+      body: tabs[selectedIndex],
+      bottomNavigationBar: BottomNav(
+        selectedIndex: selectedIndex,
+        onTap: (value) {
+          setState(() {
+            selectedIndex = value;
+          });
+        },
+      ),
     );
   }
 }
---
+++ lib/widgets/bottom_nav.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:hero_location/core/utils/app_colors.dart';
+
+class BottomNav extends StatelessWidget {
+  const BottomNav({super.key, required this.selectedIndex, this.onTap});
+  final int selectedIndex;
+  final void Function(int)? onTap;
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      backgroundColor: AppColors.primryColor,
+      selectedItemColor: Colors.white,
+      unselectedItemColor: Colors.black,
+      currentIndex: selectedIndex,
+      showSelectedLabels: true,
+      showUnselectedLabels: false,
+      onTap: onTap,
+
+      items: const [
+        BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+        BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
+      ],
+    );
+  }
+}
