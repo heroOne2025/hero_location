aede2c611581e1b64769358c91cbadef8f08d2eb
abdelrahman <hone83344@gmail.com>
2025-10-05 16:05:21 +0300

    feat: refactor AddClintLocationForm to use StatefulWidget and implement form validation

--- lib/screens/Add_clint_location_screen.dart
+++ lib/screens/Add_clint_location_screen.dart
@@ -2,9 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:hero_location/core/utils/app_colors.dart';
 import 'package:hero_location/widgets/add_clint_location_form.dart';
-import 'package:hero_location/widgets/custom_text_form_field.dart';
-
-import '../widgets/custom_elevated_button.dart';
 
 class AddClintLocationScreen extends StatelessWidget {
   static String routeName = '/add_clint_location';
--- lib/widgets/add_clint_location_form.dart
+++ lib/widgets/add_clint_location_form.dart
@@ -1,66 +1,96 @@
 import 'package:flutter/material.dart';
+import 'package:hero_location/core/utils/app_validator.dart';
 import 'package:hero_location/widgets/custom_elevated_button.dart';
 import 'package:hero_location/widgets/custom_text_form_field.dart';
 
-class AddClintLocationForm extends StatelessWidget {
+class AddClintLocationForm extends StatefulWidget {
   const AddClintLocationForm({super.key});
 
   @override
+  State<AddClintLocationForm> createState() => _AddClintLocationFormState();
+}
+
+class _AddClintLocationFormState extends State<AddClintLocationForm> {
+  TextEditingController nameController = TextEditingController();
+  TextEditingController phoneController = TextEditingController();
+  TextEditingController adressController = TextEditingController();
+  GlobalKey<FormState> formKey = GlobalKey();
+  AutovalidateMode autovalidateMode = AutovalidateMode.disabled;
+  bool isLoading = false;
+
+  @override
   Widget build(BuildContext context) {
-    return Column(
-      // mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: [
-        Container(
-          margin: const EdgeInsets.all(30),
-          height: 175,
-          width: 100,
-          color: Colors.grey[400],
-          child: const Center(
-            child: Icon(Icons.map, size: 50, color: Colors.white),
+    return Form(
+      key: formKey,
+      autovalidateMode: autovalidateMode,
+      child: Column(
+        // mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Container(
+            margin: const EdgeInsets.all(30),
+            height: 175,
+            width: 100,
+            color: Colors.grey[400],
+            child: const Center(
+              child: Icon(Icons.map, size: 50, color: Colors.white),
+            ),
           ),
-        ),
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 80),
-          child: CustomElevatedButton(
-            textOnButton: 'Get Current Location',
-            onPressed: () {},
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 80),
+            child: CustomElevatedButton(
+              textOnButton: 'Get Current Location',
+              onPressed: () {},
+            ),
           ),
-        ),
-        const SizedBox(height: 20),
-        Text('name'),
-        SizedBox(height: 8),
-        CustomTextFormField(
-          hintText: "Clint Name",
-          labelText: 'name',
-          keyboardType: TextInputType.name,
-        ),
-        SizedBox(height: 10),
-        Text('phone'),
-        SizedBox(height: 8),
-        CustomTextFormField(
-          hintText: "Clint Phone",
-          labelText: 'phone',
-          keyboardType: TextInputType.phone,
-        ),
-        SizedBox(height: 10),
-        Text('address'),
-        SizedBox(height: 8),
-        CustomTextFormField(
-          hintText: "Clint Address",
-          labelText: 'address',
-          keyboardType: TextInputType.streetAddress,
-        ),
-        SizedBox(height: 20),
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 80),
-          child: CustomElevatedButton(
-            textOnButton: 'Add Clint',
-            onPressed: () {},
+          const SizedBox(height: 20),
+          Text('name'),
+          SizedBox(height: 8),
+          CustomTextFormField(
+            hintText: "Clint Name",
+            labelText: 'name',
+            keyboardType: TextInputType.name,
+            controller: nameController,
+            validator: (value) => AppValidator.validateName(value),
           ),
-        ),
-        SizedBox(height: 20),
-      ],
+          SizedBox(height: 10),
+          Text('phone'),
+          SizedBox(height: 8),
+          CustomTextFormField(
+            hintText: "Clint Phone",
+            labelText: 'phone',
+            keyboardType: TextInputType.phone,
+            controller: phoneController,
+            validator: (value) => AppValidator.validatePhone(value),
+          ),
+          SizedBox(height: 10),
+          Text('address'),
+          SizedBox(height: 8),
+          CustomTextFormField(
+            hintText: "Clint Address",
+            labelText: 'address',
+            keyboardType: TextInputType.streetAddress,
+            validator: (value) => AppValidator.validateAddress(value),
+            controller: adressController,
+          ),
+          SizedBox(height: 20),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 80),
+            child: CustomElevatedButton(
+              textOnButton: 'Add Clint',
+              onPressed: () {
+                if (formKey.currentState!.validate()) {
+                } else {
+                  setState(() {
+                    autovalidateMode = AutovalidateMode.always;
+                  });
+                }
+              },
+            ),
+          ),
+          SizedBox(height: 20),
+        ],
+      ),
     );
   }
 }
